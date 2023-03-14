6af20763c3c12ff52adcd37d5d1599c7e7a8bef3
minmincute912 <86720631+minmincute912@users.noreply.github.com>
2023-03-14 11:11:35 +0700

    Update exam.py

--- exam.py
+++ exam.py
@@ -1,24 +1,27 @@
+# su dung PST de tim kiem du lieu trong khoang
+# khai bao 1 node
+
 class Node:
     def __init__(self, point):
         self.point = point
         self.left = None
         self.right = None
-
+# khai bao cau truc du lieu prioritysearchtree
 class PrioritySearchTree:
     def __init__(self, points):
         self.root = self.build_tree(points)
-
+# lenh tao cay
     def build_tree(self, points):
         if not points:
             return None
         else:
-            points.sort(key=lambda x: x[0])
-            mid = len(points) // 2
+            points.sort(key=lambda x: x[0])  #sap xep cac gia tri uu tien
+            mid = len(points) // 2    #tim kiem trung vi
             node = Node(points[mid])
             node.left = self.build_tree(points[:mid])
             node.right = self.build_tree(points[mid+1:])
             return node
-
+# ham them node vao cay
     def add_node(self, point, node):
         if not node:
             return Node(point)
@@ -27,7 +30,7 @@ class PrioritySearchTree:
         else:
             node.right = self.add_node(point, node.right)
         return node
-
+# ham tim kiem du lieu quan trong
     def search_range(self, x1, x2, node, results):
         if not node:
             return
